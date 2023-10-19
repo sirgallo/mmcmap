a2ebd8617b738eac37e12ac33cd9cab7ab179bfb
Sir Gallo <nickgallo97@gmail.com>
2023-10-19 19:50:56 -0400

    update tests

--- tests/MMCMapConcurrent_test.go
+++ tests/MMCMapConcurrent_test.go
@@ -2,7 +2,6 @@ package mmcmaptests
 
 import "bytes"
 import "crypto/rand"
-import "fmt"
 import "os"
 import "path/filepath"
 import "sync"
@@ -35,23 +34,19 @@ func init() {
 		panic(initPCMapErr.Error())
 	}
 
-	inputSize = 100000
+	inputSize =100000
 	keyValPairs = make([]KeyVal, inputSize)
 
 	for idx := range keyValPairs {
 		randomBytes, _ := GenerateRandomBytes(32)
 		keyValPairs[idx] = KeyVal{Key: randomBytes, Value: randomBytes}
 	}
-
-	fmt.Println("seeded keyValPairs array:", inputSize)
 }
 
 func GenerateRandomBytes(length int) ([]byte, error) {
 	randomBytes := make([]byte, length)
 	_, err := rand.Read(randomBytes)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 
 	for i := 0; i < length; i++ {
 		randomBytes[i] = 'a' + (randomBytes[i] % 26)
@@ -74,9 +69,7 @@ func TestPCMapConcurrentOperations(t *testing.T) {
 				defer insertWG.Done()
 
 				_, putErr := concurrentPcMap.Put(val.Key, val.Value)
-				if putErr != nil {
-					t.Errorf("error on mmcmap put: %s", putErr.Error())
-				}
+				if putErr != nil { t.Errorf("error on mmcmap put: %s", putErr.Error()) }
 			}(val)
 		}
 
@@ -94,12 +87,9 @@ func TestPCMapConcurrentOperations(t *testing.T) {
 				defer retrieveWG.Done()
 
 				value, getErr := concurrentPcMap.Get(val.Key)
-				if getErr != nil {
-					t.Errorf("error on mmcmap get: %s", getErr.Error())
-				}
+				if getErr != nil { t.Errorf("error on mmcmap get: %s", getErr.Error()) }
 
-				// t.Logf("actual: %s, expected: %s", value, val.Value)
-				if !bytes.Equal(value, val.Value) {
+				if ! bytes.Equal(value, val.Value) {
 					t.Errorf("actual value not equal to expected: actual(%s), expected(%s)", value, val.Value)
 				}
 			}(val)
@@ -119,9 +109,7 @@ func TestPCMapConcurrentOperations(t *testing.T) {
 				defer retrieveWG.Done()
 
 				_, delErr := concurrentPcMap.Delete(val.Key)
-				if delErr != nil {
-					t.Errorf("error on mmcmap delete: %s", delErr.Error())
-				}
+				if delErr != nil { t.Errorf("error on mmcmap delete: %s", delErr.Error()) }
 			}(val)
 		}
 
@@ -131,9 +119,7 @@ func TestPCMapConcurrentOperations(t *testing.T) {
 
 	t.Run("MMCMap File Size", func(t *testing.T) {
 		fSize, sizeErr := concurrentPcMap.FileSize()
-		if sizeErr != nil {
-			t.Errorf("error getting file size: %s", sizeErr.Error())
-		}
+		if sizeErr != nil { t.Errorf("error getting file size: %s", sizeErr.Error()) }
 
 		t.Log("File Size In Bytes:", fSize)
 	})
